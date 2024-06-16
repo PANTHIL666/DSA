28e07fee157d5302cc5bad51188976718c116b54
PANTHIL666 <panthilkankotiya2003@gmail.com>
2024-06-16 09:47:50 +0530

    binaryTree travarsal operation

--- src/binaryTree.java
+++ src/binaryTree.java
@@ -31,8 +31,44 @@ public class binaryTree {
         return root;
     }
 
+    static void inorderTraversal(Node root) {
+        //follows LNR order
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left);
+        System.out.print(root.data + " ");
+        inorderTraversal(root.right);
+    }
+    static void preorderTraversal(Node root) {
+        //follows NLR order
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+    }
+
+    static void postorderTraversal(Node root) {
+        if (root == null) {
+            return;
+        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        System.out.print(root.data + " ");
+    }
+
+
+
     public static void main(String[] args) {
         Node root = createBinaryTree();
+        inorderTraversal(root);
+        System.out.println("        ");
+        preorderTraversal(root);
+        System.out.println("        ");
+        postorderTraversal(root);
+
         // Optionally, close the scanner if you want
         // sc.close();
     }
